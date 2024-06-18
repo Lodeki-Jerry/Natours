92aa4ac3890a02e82b9b62b0c3e518a0eb51acbc
Lodeki-Jerry <elodeki@gmail.com>
2024-06-18 18:56:04 +0300

    'initial commit'

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const morgan = require('morgan');
+
+const tourRouter = require('./Routes/tourRoutes');
+
+const userRouter = require('./Routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
