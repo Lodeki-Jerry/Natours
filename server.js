99b1020c6b42350c3847297c589da8a503e917a9
Lodeki-Jerry <elodeki@gmail.com>
2024-06-28 18:45:16 +0300

    Third Commit

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
@@ -5,6 +7,18 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 // console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
 
