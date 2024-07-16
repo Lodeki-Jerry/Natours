202290b0e3fce991aaab1a620c1029336018786e
Lodeki-Jerry <elodeki@gmail.com>
2024-07-16 15:19:10 +0300

    API Update

--- app.js
+++ app.js
@@ -26,4 +26,28 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'Fail',
+  //   message: `Cannot find the ${req.originalUrl} on this server`,
+  // });
+
+  const err = new Error(`Cannot find the ${req.originalUrl} on the server`);
+  err.status = 'Fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
