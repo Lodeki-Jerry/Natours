8ac30a144bfb14628b98bc190d7ee3d991067a71
Lodeki-Jerry <elodeki@gmail.com>
2024-07-16 16:04:39 +0300

    API UPDATE

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+
+const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./Routes/tourRoutes');
 
 const userRouter = require('./Routes/userRoutes');
@@ -28,26 +32,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'Fail',
-  //   message: `Cannot find the ${req.originalUrl} on this server`,
-  // });
-
-  const err = new Error(`Cannot find the ${req.originalUrl} on the server`);
-  err.status = 'Fail';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Cannot find the ${req.originalUrl} on the server`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
