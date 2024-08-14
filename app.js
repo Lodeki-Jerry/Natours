540f4b9a60cfb8db83d7b86ab729a24443593072
Lodeki-Jerry <elodeki@gmail.com>
2024-08-14 17:34:09 +0300

    Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const morgan = require('morgan');
 
+const rateLimit = require('express-rate-limit');
+
+const helmet = require('helmet');
+
+const mongoSanitize = require('express-mongo-sanitize');
+
+const xss = require('xss-clean');
+
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 
 const globalErrorHandler = require('./controllers/errorController');
@@ -13,12 +23,40 @@ const userRouter = require('./Routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
+app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. please try again later in an hour',
+});
+
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+// Data Sanitization against noSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against xss
+app.use(xss());
+// Preventing parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
