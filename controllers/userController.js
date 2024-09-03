535f72a00cfa9ddf8e228c2413b7974f2ed7fa17
Lodeki-Jerry <elodeki@gmail.com>
2024-09-03 14:52:06 +0300

    Factory handlers

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,18 +13,6 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users,
-    },
-  });
-});
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) create error if the user posts password update
   if (req.body.password || req.body.passwordConfirm) {
@@ -58,19 +46,13 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'err',
-    message: 'This route is not implemented',
-  });
-};
-
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'err',
     message: 'This route is not implemented',
   });
 };
-
+exports.getUser = factory.getOne(User);
+exports.getAllUsers = factory.getAll(User);
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
